28e79f7f9a88a1bfea330d67cc410bf6b115aa86
amite <amit.erandole@gmail.com>
2013-03-23 21:54:28 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liveblog::Application.config.secret_token = 'f7a4e858fcd2f94451a421aec4d1120eb0418c2af21823ee780904d00bfbdb3bbaf967197542aabd41e7987b1b30e45f4f1b6eb49db23dace0248feb66027845'
